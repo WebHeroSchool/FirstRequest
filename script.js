7be3c12d49168e868de4010bc1a3ef8cf428dd50
Никита Родионов <nikita.rodionov.job@yandex.ru>
2019-08-11 13:11:05 +0300

    new request

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const body = document.body;
+const url = window.location.toString();
+const date = new Date();
+const loader = document.getElementById('loader');
 
-const url = window.location.toString()
-const getUsername = (url) => {
+const showLoader = () => {
+	loader.style.display = 'none';
+}
+
+const getUsernameFromUrl = (url) => {
 	let splitOfUrl = url.split('=');
 	let stringOfUsername  = splitOfUrl[1];
 	if (stringOfUsername == undefined) {
@@ -9,30 +15,46 @@ const getUsername = (url) => {
 	}
 	return stringOfUsername;
 }
-getUsername(url);
-fetch(`https://api.github.com/users/${getUsername(url)}`)
-	.then(res => res.json())
-	.then(user => {
-		avatarOfUser = user.avatar_url;
-		bioOfUser = user.bio;
-		urlOfUser = user.url;
-		const addUser = () => {
-			const user = document.createElement('h1');
-			user.innerHTML = `${getUsername(url)}`;
-			body.appendChild(user);
-		}
-		const addBio = () => {
-			const bio = document.createElement('p');
-			bio.innerHTML = `${bioOfUser}`;
-			body.appendChild(bio);
-		}
-		const addImg = () => {
-			const img = document.createElement('img');
-			img.src = this.avatarOfUser;
-			body.appendChild(img);
-		}
-		addUser();
-		addBio();
-		addImg();
+
+const getDate = new Promise((resolve, reject) =>
+	setTimeout(() => date ? resolve(date) : reject('Время неизвестно'), 2000)
+);
+
+const getRequest = fetch(`https://api.github.com/users/${getUsernameFromUrl(url)}`);
+
+Promise.all([getRequest, getDate])
+	.then(([request, date]) => {
+		requestFromPromise = request;
+		dateFromPromise = date;
 	})
+  .then(res => requestFromPromise.json())
+	.then(user => {
+    avatarOfUser = user.avatar_url;
+    bioOfUser = user.bio;
+    urlOfUser = user.url;
+    const addUser = () => {
+      const user = document.createElement('h1');
+      user.innerHTML = `${getUsernameFromUrl(url)}`;
+      body.appendChild(user);
+    }
+    const addBio = () => {
+      const bio = document.createElement('p');
+      bio.innerHTML = `${bioOfUser}`;
+      body.appendChild(bio);
+    }
+    const addImg = () => {
+      const img = document.createElement('img');
+      img.src = this.avatarOfUser;
+      body.appendChild(img);
+    }
+    addUser();
+    addBio();
+    addImg();
+  })
+  .then(res => {
+  	const date = document.createElement('p');
+  	date.innerHTML = `${dateFromPromise}`;
+  	body.appendChild(date);
+  	showLoader();
+  })
 	.catch(err => alert('Информация о пользователе не доступна'));
